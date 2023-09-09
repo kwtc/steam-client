18659e885e4f5440570f2c55021071f21524e29e
Kasper Christiansen <kasper@kwc.dk>
2023-09-09 08:31:50 +0200

    Added converters for remaining dynamic types

--- src/Kwtc.Steam.Client/Models/Details/SteamAppDetailDataModel.cs
+++ src/Kwtc.Steam.Client/Models/Details/SteamAppDetailDataModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Kwtc.Steam.Client.Converters;
 
 namespace Kwtc.Steam.Client.Models.Details;
 
@@ -16,6 +17,7 @@ public class SteamAppDetailDataModel
     public int? Id { get; set; }
 
     [JsonPropertyName("required_age")]
+    [JsonConverter(typeof(NumberOrStringToStringConverter))]
     public string? RequiredAge { get; set; }
 
     [JsonPropertyName("is_free")]
@@ -23,11 +25,11 @@ public class SteamAppDetailDataModel
 
     [JsonPropertyName("controller_support")]
     public string? ControllerSupport { get; set; }
-    
+
     [JsonPropertyName("dlc")]
     [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
     public IEnumerable<int>? Dlc { get; set; }
-    
+
     [JsonPropertyName("detailed_description")]
     public string? DetailedDescription { get; set; }
 
@@ -45,40 +47,34 @@ public class SteamAppDetailDataModel
 
     [JsonPropertyName("header_image")]
     public string? HeaderImage { get; set; }
-    
+
     [JsonPropertyName("capsule_image")]
     public string? CapsuleImage { get; set; }
-    
+
     [JsonPropertyName("capsule_imagev5")]
     public string? CapsuleImageV5 { get; set; }
 
     [JsonPropertyName("website")]
     public string? Website { get; set; }
-    
-    /// <summary>
-    /// This is dynamic because it can be an empty array (if not defined) or an object ¯\_(ツ)_/¯.
-    /// </summary>
+
     [JsonPropertyName("pc_requirements")]
-    public dynamic? PcRequirements { get; set; }
+    [JsonConverter(typeof(SteamAppRequirementsConverter))]
+    public SteamAppRequirementsModel? PcRequirements { get; set; }
 
-    /// <summary>
-    /// This is dynamic because it can be an empty array (if not defined) or an object ¯\_(ツ)_/¯.
-    /// </summary>
     [JsonPropertyName("mac_requirements")]
-    public dynamic? MacRequirements { get; set; }
+    [JsonConverter(typeof(SteamAppRequirementsConverter))]
+    public SteamAppRequirementsModel? MacRequirements { get; set; }
 
-    /// <summary>
-    /// This is dynamic because it can be an empty array (if not defined) or an object ¯\_(ツ)_/¯.
-    /// </summary>
     [JsonPropertyName("linux_requirements")]
-    public dynamic? LinuxRequirements { get; set; }
+    [JsonConverter(typeof(SteamAppRequirementsConverter))]
+    public SteamAppRequirementsModel? LinuxRequirements { get; set; }
 
     [JsonPropertyName("legal_notice")]
     public string? LegalNotice { get; set; }
-    
+
     [JsonPropertyName("drm_notice")]
     public string? DrmNotice { get; set; }
-    
+
     [JsonPropertyName("ext_user_account_notice")]
     public string? ExistingUserAccountNotice { get; set; }
 
@@ -86,11 +82,11 @@ public class SteamAppDetailDataModel
     public IEnumerable<string>? Developers { get; set; }
 
     /// <summary>
-    /// Can actually contain a single empty string value (I'm guessing if the app is self published) ¯\_(ツ)_/¯.
+    /// Can actually contain a single empty string value or a string containing a whitespace (I'm guessing if the app is self published) ¯\_(ツ)_/¯.
     /// </summary>
     [JsonPropertyName("publishers")]
     public IEnumerable<string>? Publishers { get; set; }
-    
+
     [JsonPropertyName("demos")]
     public IEnumerable<SteamAppDemoDescriptionModel>? Demos { get; set; }
 
@@ -115,16 +111,16 @@ public class SteamAppDetailDataModel
 
     [JsonPropertyName("screenshots")]
     public IEnumerable<SteamAppScreenshotModel>? Screenshots { get; set; }
-    
+
     [JsonPropertyName("movies")]
     public IEnumerable<SteamAppMovieModel>? Movies { get; set; }
 
     [JsonPropertyName("recommendations")]
     public SteamAppRecommendationsModel? Recommendations { get; set; }
-    
+
     [JsonPropertyName("achievements")]
     public SteamAppAchievementsModel? Achievements { get; set; }
-    
+
     [JsonPropertyName("release_date")]
     public SteamAppReleaseDateModel? ReleaseDate { get; set; }
 
@@ -133,7 +129,7 @@ public class SteamAppDetailDataModel
 
     [JsonPropertyName("background")]
     public string? BackgroundUrl { get; set; }
-    
+
     [JsonPropertyName("background_raw")]
     public string? BackgroundUrlRaw { get; set; }
 
