22f175e13fc94cefdf2ce58534d57024dd5a4442
Kasper Christiansen <kasper@kwc.dk>
2024-11-19 06:48:43 +0100

    Added parameters, config and unit tests

--- src/Kwtc.Steam.Client/ISteamClient.cs
+++ src/Kwtc.Steam.Client/ISteamClient.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Kwtc.Steam.Client.Models.Details;
@@ -7,11 +8,51 @@ namespace Kwtc.Steam.Client;
 
 public interface ISteamClient
 {
-    Task<SteamAppDetailRootModel?> GetAppDetailsAsync(string id, CancellationToken cancellationToken = default);
-    
-    Task<string?> GetAppDetailsJsonAsync(string id, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Get the details of one or more Steam apps mapped to a model.
+    /// </summary>
+    /// <param name="ids">
+    /// A collection of Steam app ids to get details for.
+    /// The Steam API only supports multiple ids in combination with the <see cref="SteamFilters.PriceOverview"/> filter.
+    /// </param>
+    /// <param name="countryCode">Optional country code affecting prices.</param>
+    /// <param name="language">Optional language.</param>
+    /// <param name="filters">Optional collection of filters determining the details returned.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    Task<Dictionary<string, SteamAppDetailRootModel>> GetAppDetailsAsync(
+        List<string> ids,
+        string? countryCode,
+        string? language,
+        List<string>? filters,
+        CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Get the details of a Steam app as a json string.
+    /// </summary>
+    /// /// <param name="ids">
+    /// A collection of Steam app ids to get details for.
+    /// The Steam API only supports multiple ids in combination with the <see cref="SteamFilters.PriceOverview"/> filter.
+    /// </param>
+    /// <param name="countryCode">Optional country code affecting prices.</param>
+    /// <param name="language">Optional language.</param>
+    /// <param name="filters">Optional collection of filters determining the details returned.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    Task<string?> GetAppDetailsJsonAsync(
+        List<string> ids,
+        string? countryCode,
+        string? language,
+        List<string>? filters,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets a list of all Steam apps mapped to a model.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task<SteamAppListRootModel?> GetAppListAsync(CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Gets a list of all Steam apps as a json string.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
     Task<string?> GetAppListJsonAsync(CancellationToken cancellationToken = default);
 }
---
+++ src/Kwtc.Steam.Client/SteamFilters.cs
@@ -0,0 +1,19 @@
+namespace Kwtc.Steam.Client;
+
+public class SteamFilters
+{
+    public const string PriceOverview = "price_overview";
+    public const string Packages = "packages";
+    public const string Developers = "developers";
+    public const string Publishers = "publishers";
+    public const string Platforms = "platforms";
+    public const string MetaCritic = "metacritic";
+    public const string Categories = "categories";
+    public const string Genres = "genres";
+    public const string Screenshots = "screenshots";
+    public const string Recommendations = "recommendations";
+    public const string ReleaseDate = "release_date";
+    public const string SupportInfo = "support_info";
+    public const string ContentDescriptors = "content_descriptors";
+    public const string Ratings = "ratings";
+}
